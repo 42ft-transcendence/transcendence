b654c7636378ca8d4aa973121573522948500f6f
42sangkkim <sangkkim@student.42seoul.kr>
2023-08-24 18:11:15 +0900

    Feat: auth 페이지 생성 #20

---
+++ frontend/react-app/src/pages/auth/index.tsx
@@ -0,0 +1,11 @@
+import Loading from "@assets/images/loading.gif";
+
+const AuthPage = () => {
+  return (
+    <>
+      <img src={Loading} alt="loading" />
+    </>
+  );
+};
+
+export default AuthPage;
