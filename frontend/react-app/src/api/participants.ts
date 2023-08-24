76bf7d9ff2aa8e5c256b503601a248e5c7b38ea2
Y2IN <dpdls0228@daum.net>
2023-08-24 19:29:21 +0900

    Feat: api 추가#10

---
+++ frontend/react-app/src/api/participants.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosResponse } from "axios";
+import * as cookies from "react-cookies";
+import { base_url } from "./index";
+import { ChannelType, ParticipantType } from "@type";
+
+/**
+ * 내가 참여한 채팅방 리스트 요청
+ * @url /participants/rooms
+ * @method GET
+ * @returns 내가 참여한 채팅방 리스트 반환
+ */
+export const getMyChannels = async (): Promise<
+  AxiosResponse<ChannelType[]>
+> => {
+  const response = await axios.get(`${base_url}/participants/rooms`, {
+    headers: {
+      Authorization: `Bearer ${cookies.load("jwt") as string}`,
+    },
+  });
+  return response;
+};
+
+/**
+ * 채팅방 참가자 리스트 요청
+ * @url /participants/participants?roomId=roomId
+ * @method GET
+ * @param roomId 채팅방 아이디
+ * @returns 채팅방 참가자 리스트 반환
+ */
+export const getParticipantsByRoomId = async (
+  roomId: string,
+): Promise<AxiosResponse<ParticipantType[]>> => {
+  const response = await axios.get(
+    `${base_url}/participants/participants?roomId=${roomId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${cookies.load("jwt") as string}`,
+      },
+    },
+  );
+  return response;
+};
