683b7f33ea94d87aede1d5ce3b0b57fab24ac89d
Chanwoong1 <jcw7468@gmail.com>
2023-08-25 08:53:47 +0900

    Chore: 프론트 환경 설정 수정 #28

--- frontend/react-app/vite.config.ts
+++ frontend/react-app/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), EnvironmentPlugin("all", { prefix: "VITE_" })],
   resolve: {
     alias: [
       { find: "@src", replacement: resolve(__dirname, "src") },
