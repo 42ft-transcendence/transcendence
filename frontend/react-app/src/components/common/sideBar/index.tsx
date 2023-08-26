f80c8414efdc8b3d418ac1f1e86202678899a7b6
Chanwoong1 <jcw7468@gmail.com>
2023-08-26 20:04:08 +0900

    Refactor: profileRouteMatch 코드 분리 #45

--- frontend/react-app/src/components/common/sideBar/index.tsx
+++ frontend/react-app/src/components/common/sideBar/index.tsx
@@ -1,4 +1,4 @@
-import { SidebarConfigType } from "@src/types";
+import { SidebarComponentType, SidebarConfigType } from "@src/types";
 import chattingSideBar from "./chattingSideBar";
 import ProfileSideBar from "./profileSideBar";
 
@@ -10,6 +10,26 @@ export const sidebarConfig: SidebarConfigType = {
   "/": { component: chattingSideBar },
   "/profile/": {
     component: ProfileSideBar,
-    matcher: /^\/profile\/.+$/, // 이 정규식은 /profile/ 다음에 어떤 문자열이든 매치합니다.
+    matcher: /^\/profile\/(42-|G-)\d+$/, // '42-' 혹은 'G-'로 시작하고 그 뒤에 숫자로만 이루어진 문자열이 나오는 경우만 매치합니다.
   },
+};
+
+export const profileRouteMatch = (
+  currentRoute: string,
+): SidebarComponentType | null => {
+  for (const key in sidebarConfig) {
+    const config = sidebarConfig[key];
+
+    // 만약 matcher가 있고 currentRoute와 일치한다면
+    if (config.matcher?.test(currentRoute)) {
+      return config.component;
+    }
+
+    // 만약 key와 currentRoute가 정확히 일치한다면
+    if (key === currentRoute && key !== "/profile/") {
+      return config.component;
+    }
+  }
+
+  return null; // 일치하는 라우트가 없을 때
 };
--- frontend/react-app/src/pages/profile/index.tsx
+++ frontend/react-app/src/pages/profile/index.tsx
@@ -1,26 +1,11 @@
 import NavBar from "@src/components/common/navBar";
-import { sidebarConfig } from "@src/components/common/sideBar";
-import { SidebarComponentType } from "@src/types";
-
-const matchRoute = (currentRoute: string): SidebarComponentType | null => {
-  for (const key in sidebarConfig) {
-    const config = sidebarConfig[key];
-    if (config.matcher) {
-      if (config.matcher.test(currentRoute)) {
-        return config.component;
-      }
-    } else if (key === currentRoute) {
-      return config.component;
-    }
-  }
-  return null;
-};
+import { profileRouteMatch } from "@src/components/common/sideBar";
 
 const Profile = () => {
   const currentRoute = window.location.pathname;
 
   console.log("currentRoute", currentRoute);
-  const SidebarComponent = matchRoute(currentRoute);
+  const SidebarComponent = profileRouteMatch(currentRoute);
 
   return (
     <>
