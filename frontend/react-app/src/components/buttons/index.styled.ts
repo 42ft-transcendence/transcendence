c5826322e5a4eaad2213f9349678a25574feb869
Chanwoong1 <jcw7468@gmail.com>
2023-08-27 20:43:29 +0900

    Design: 버튼의 DarkMode 색상 변경 #55

--- frontend/react-app/src/components/buttons/index.styled.ts
+++ frontend/react-app/src/components/buttons/index.styled.ts
@@ -7,25 +7,25 @@ export const IconButton = styled.button<{ mode: "LIGHT" | "DARK" }>`
   background-color: ${(props) =>
     props.mode === "LIGHT"
       ? props.theme.colors.freezePurple
-      : props.theme.colors.heavyPurple};
+      : props.theme.colors.iceCold};
   border-radius: 10px;
   box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.25);
   outline: none;
   border: none;
   justify-content: center;
   align-items: center;
-  color: ${(props) =>
-    props.mode === "LIGHT"
-      ? props.theme.colors.heavyPurple
-      : props.theme.colors.freezePurple};
   font-weight: Bold;
   text-decoration: none;
-
+  color: ${(props) => props.theme.colors.heavyPurple};
   // 아이콘과 텍스트 사이의 간격
   & > *:nth-child(even) {
     margin-left: 4px;
   }
 `;
+// color: ${(props) =>
+//   props.mode === "LIGHT"
+//     ? props.theme.colors.heavyPurple
+//     : props.theme.colors.freezePurple};
 
 export const ButtonList = styled.div`
   display: flex;
