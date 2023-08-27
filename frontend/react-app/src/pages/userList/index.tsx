4ccf57a161c1a9091cb2e779c4c8fd9806b1ba1a
Chanwoong1 <jcw7468@gmail.com>
2023-08-27 18:10:58 +0900

    Feat: 서치바에 입력된 텍스트 받아올 수 있도록 함 #55

--- frontend/react-app/src/pages/userList/index.tsx
+++ frontend/react-app/src/pages/userList/index.tsx
@@ -3,13 +3,18 @@ import { sidebarConfig } from "@src/components/common/sideBar";
 import * as DS from "../index.styled";
 import * as S from "./index.styled";
 import SearchIcon from "@assets/icons/MagnifyingGlass.svg";
+import { useEffect, useState } from "react";
 
 const UserList = () => {
+  const [search, setSearch] = useState<string>("");
   const currentRoute = window.location.pathname;
-
-  console.log("currentRoute", currentRoute);
   const CurrentSideBar = sidebarConfig[currentRoute];
   const CurrentSideBarComponent = CurrentSideBar.component;
+  console.log("currentRoute", currentRoute);
+
+  useEffect(() => {
+    console.log("search", search);
+  }, [search]);
 
   return (
     <>
@@ -18,7 +23,14 @@ const UserList = () => {
       <DS.ContentArea>
         <S.SearchBarContainer>
           <S.SearchBar>
-            <S.SearchBarInput placeholder="유저 닉네임 검색" maxLength={10} />
+            <S.SearchBarInput
+              type="text"
+              placeholder="유저 닉네임 검색"
+              maxLength={10}
+              id="nickname"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
             <S.SearchBarImg src={SearchIcon} />
           </S.SearchBar>
         </S.SearchBarContainer>
