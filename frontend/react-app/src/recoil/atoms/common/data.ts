1fa2f7a4d2fd35793633246685fa3cbb8fc5e98e
Chanwoong1 <jcw7468@gmail.com>
2023-08-27 20:16:42 +0900

    Fix: 더미 데이터 유저의 점수 산정 방식 변경 #55

--- frontend/react-app/src/recoil/atoms/common/data.ts
+++ frontend/react-app/src/recoil/atoms/common/data.ts
@@ -17,8 +17,10 @@ export const initialUserData = {
 
 const K = 32;
 
-const expectedScore = (ratingA: number, ratingB = 1000): number =>
-  1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
+const expectedScore = (
+  ratingA: number,
+  ratingB = Math.floor(Math.random() * 1000) + 1200,
+): number => 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
 
 const updateRating = (
   rating: number,
@@ -48,13 +50,13 @@ const calculateRating = (
 
 export const createDummyUsers = (num: number): UserType[] => {
   return Array.from({ length: num }, (_, index): UserType => {
-    const id = `42-10000${index + 1}`;
-    const nickname = `user${Math.floor(Math.random() * 9) + 2}`;
+    const id = `42-${(index + 1).toString().padStart(6, "0")}`;
+    const nickname = `user${index + 1}`;
     const win = Math.floor(Math.random() * 201);
     const lose = Math.floor(Math.random() * 201);
     const ladder_win = Math.floor(Math.random() * 201);
     const ladder_lose = Math.floor(Math.random() * 201);
-    const rating = calculateRating(1000, ladder_win, ladder_lose);
+    const rating = Math.floor(calculateRating(1000, ladder_win, ladder_lose));
     const status = Math.floor(Math.random() * 3); // 랜덤하게 0, 1, 2 중 하나를 선택
 
     return {
