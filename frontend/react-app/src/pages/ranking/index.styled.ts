66f20d20e70529ee0df4e001416ebc266e3e77ac
Chanwoong1 <jcw7468@gmail.com>
2023-08-27 22:57:49 +0900

    Feat: rating 순으로 정렬하여 컨텐츠 영역에 표시 #59

--- frontend/react-app/src/pages/ranking/index.styled.ts
+++ frontend/react-app/src/pages/ranking/index.styled.ts
@@ -24,10 +24,49 @@ export const SearchContainer = styled.div`
 
 export const RankingContainer = styled.div`
   width: 100%;
-  height: calc(100% - 120px);
+  height: calc(100% - 120px); // height 대신 min-height를 사용
   display: flex;
   margin-top: 30px;
   background-color: ${(props) => props.theme.colors.lose};
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  padding: 20px;
+  overflow-y: scroll;
+`;
+
+export const UserCard = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
+  border-bottom: 1px solid #e1e1e1;
+`;
+
+export const Rank = styled.span`
+  width: 30px;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const ProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
+
+export const Nickname = styled.span`
+  flex: 1;
+  font-size: 18px;
+`;
+
+export const Tier = styled.span`
+  width: 150px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Record = styled.span`
+  width: 100px;
+  font-size: 16px;
 `;
--- frontend/react-app/src/pages/ranking/index.tsx
+++ frontend/react-app/src/pages/ranking/index.tsx
@@ -2,13 +2,19 @@ import NavBar from "@src/components/common/navBar";
 import { sidebarConfig } from "@src/components/common/sideBar";
 import * as DS from "../index.styled";
 import * as S from "./index.styled";
+import { useRecoilState } from "recoil";
+import { allUserListState } from "@src/recoil/atoms/common";
+import { useState } from "react";
 
 const Ranking = () => {
   const currentRoute = window.location.pathname;
-
-  console.log("currentRoute", currentRoute);
   const CurrentSideBar = sidebarConfig[currentRoute];
   const CurrentSideBarComponent = CurrentSideBar.component;
+
+  const [userList] = useRecoilState(allUserListState);
+  const [sortedUserList] = useState(
+    [...userList].sort((a, b) => b.rating - a.rating),
+  );
 
   return (
     <>
@@ -20,7 +26,22 @@ const Ranking = () => {
           <div>toolBar area</div>
           <S.SearchContainer>SearchContainer</S.SearchContainer>
         </S.ToolBarContainer>
-        <S.RankingContainer>Ranking</S.RankingContainer>
+        <S.RankingContainer>
+          {sortedUserList.map((user, index) => (
+            <S.UserCard key={user.id}>
+              <S.Rank>{index + 1}</S.Rank>
+              <S.ProfileImage src={user.avatarPath} alt={user.nickname} />
+              <S.Nickname>{user.nickname}</S.Nickname>
+              <S.Tier>
+                {/* {user.tier}  */}
+                {user.rating} LP
+              </S.Tier>
+              <S.Record>
+                {user.ladder_win}W / {user.ladder_lose}L
+              </S.Record>
+            </S.UserCard>
+          ))}
+        </S.RankingContainer>
       </DS.ContentArea>
     </>
   );
