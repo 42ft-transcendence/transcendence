c6b0ee05836bc31cefebe09e86b92207fb715dea
Y2IN <dpdls0228@daum.net>
2023-08-28 21:23:34 +0900

    Fix: 방 제목 컴포넌트 제작 #63

--- frontend/react-app/src/components/modal/game/gameCreateModal.tsx
+++ frontend/react-app/src/components/modal/game/gameCreateModal.tsx
@@ -2,19 +2,19 @@ import Modal from "react-modal";
 import * as S from "./index.styled";
 import { useRecoilState } from "recoil";
 import { createGameRoomModalState } from "@src/recoil/atoms/modal";
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
 const GameCreateModal = () => {
   const [createGameRoom, setCreateGameRoom] = useRecoilState(
     createGameRoomModalState,
   );
 
-  // const [roomTitle, setRoomTitle] = useState("");
+  const [roomTitle, setRoomTitle] = useState("");
   // const [isEditingTitle, setIsEditingTitle] = useState(false);
 
-  // const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setRoomTitle(event.target.value);
-  // };
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomTitle(event.target.value);
+  };
 
   return (
     <Modal
@@ -25,11 +25,33 @@ const GameCreateModal = () => {
         overlay: { ...S.ModalOverlay }, // Spread 연산자 사용
       }}
     >
-      {/* <S.gameCreateModalOverlay> */}
-      {/* <S.gameCreateModalContent>하이</S.gameCreateModalContent> */}
-      {/* </S.gameCreateModalOverlay> */}
+      <S.InputBoxWrapper
+        type="text"
+        placeholder="방 제목"
+        id="nickname"
+        value={roomTitle}
+        onChange={handleTitleChange}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            // 엔터키를 누르면
+            // onKeyPress(); // 부모 컴포넌트로 엔터키를 눌렀음을 전달
+          }
+        }}
+        // 글자 수 제한
+        maxLength={23}
+      />
+      {/* <S.gameCreateModalTitle>방 만들기</S.gameCreateModalTitle>
+      <S.gameCreateModalLabel>방 제목</S.gameCreateModalLabel>
+      <S.gameCreateModalInput
+      // type="text"
+      // value={roomTitle} // 방 제목 값 연결
+      // onChange={(e) => setRoomTitle(e.target.value)} // 방 제목 변경 핸들러
+      // placeholder="방 제목을 입력하세요"
+      />
+      <S.gameCreateModalButton onClick={() => setCreateGameRoom(false)}>
+        닫기
+      </S.gameCreateModalButton> */}
     </Modal>
   );
 };
-
 export default GameCreateModal;
--- frontend/react-app/src/components/modal/game/index.styled.ts
+++ frontend/react-app/src/components/modal/game/index.styled.ts
@@ -16,29 +16,49 @@ export const ModalContent = {
   width: "700px",
   height: "550px",
   border: `2px solid ${Theme.colors.freezePurple}`,
-  display: "center",
+  display: "flex",
   flexDirection: "column",
-  justifyContent: "center",
+  // justifyContent: "center",
   alignItems: "center",
   borderRadius: "16px",
   boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-  overflow: "auto",
+  // overflow: "auto",
 };
 
+// export const gameCreateModalContent = styled.div`
+//   justifycontent: center;
+//   alignitems: center;
+//   width: 160px;
+//   min-height: 30px;
+//   left: 235px;
+//   top: auto;
+//   bottom: 425px;
+//   overflow: hidden;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
 export const gameCreateModalTitle = styled.h2`
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
+  font-size: 10px;
+  font-weight: bold;
+  font-family: inter;
+  // margin-bottom: 1px;
+  color: white;
 `;
 
 export const gameCreateModalLabel = styled.label`
-  font-size: 1rem;
-  margin-top: 1rem;
+  font-size: 10px;
+  margin-top: 10px;
+  color: white;
 `;
 
 export const gameCreateModalInput = styled.input`
   width: 100%;
   padding: 0.5rem;
   margin-top: 0.25rem;
+  color: white;
+
   border: 1px solid #ccc;
 `;
 
@@ -48,7 +68,18 @@ export const gameCreateModalButton = styled.button`
   border: none;
   padding: 0.5rem 1rem;
   cursor: pointer;
+`;
 
-  // &:hover {
-  //   background-color: #0056b3;
+export const InputBoxWrapper = styled.input`
+  width: 80%;
+  height: 40px;
+  outline: none;
+  font-size: 20px;
+  background: none;
+  text-align: center;
+  font-weight: bold;
+  border: none;
+  border-bottom: 2px solid ${Theme.colors.freezePurple};
+  color: ${Theme.colors.freezePurple};
+  margin-top: 20px;
 `;
