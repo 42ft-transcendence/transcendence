629e698c026640b1462df08605ad42613b528c93
42sangkkim <sangkkim@student.42seoul.kr>
2023-08-28 14:50:32 +0900

    Feat: joined dm list atom 생성 #46

--- frontend/react-app/src/recoil/atoms/channel/index.ts
+++ frontend/react-app/src/recoil/atoms/channel/index.ts
@@ -6,6 +6,6 @@ const { persistAtom } = recoilPersist();
 
 export const joinedChannelListState = atom<JoinedChannelType[]>({
   key: "joinedChannelListState",
-  default: [], // 최초에는 true로 설정
+  default: [],
   effects_UNSTABLE: [persistAtom],
 });
--- frontend/react-app/src/recoil/atoms/directMessage/index.ts
+++ frontend/react-app/src/recoil/atoms/directMessage/index.ts
@@ -1,11 +1,17 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
-import { DirectMessageType } from "@src/types/directMessage.type";
+import { DirectMessageType, JoinedDirectMessageType } from "@type";
 
 const { persistAtom } = recoilPersist();
 
 export const directMessageListState = atom<DirectMessageType[]>({
   key: "directMessageListState",
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const JoinedDirectMessageListState = atom<JoinedDirectMessageType[]>({
+  key: "joinedDirectMessageListState",
   default: [],
   effects_UNSTABLE: [persistAtom],
 });
