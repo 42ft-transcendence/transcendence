bf5fadae5644c9e63b2aa1d840ceab3aa60dcf16
Chanwoong1 <jcw7468@gmail.com>
2023-08-28 18:15:48 +0900

    Feat: 백엔드에서 더미유저 생성할 수 있는 함수 추가 #30

---
+++ backend/nestjs-app/src/users/createDummyUsers.ts
@@ -0,0 +1,81 @@
+import { ChatChannel } from 'src/chatting/entities/chatchannel.entity';
+
+interface DummyUser {
+  id: string;
+  nickname: string;
+  win: number;
+  lose: number;
+  ladder_win: number;
+  ladder_lose: number;
+  admin: boolean;
+  avatarPath: string;
+  status: number;
+  twoFactorAuthenticationSecret: string;
+  isTwoFactorAuthenticationEnabled: boolean;
+  rating: number;
+  channels: ChatChannel[];
+}
+
+const K = 32;
+
+const expectedScore = (
+  ratingA: number,
+  ratingB = Math.floor(Math.random() * 1000) + 1200,
+): number => 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
+
+const updateRating = (
+  rating: number,
+  expected: number,
+  actual: number,
+): number => rating + K * (actual - expected);
+
+const calculateRating = (
+  initialRating: number,
+  wins: number,
+  loses: number,
+): number => {
+  let rating = initialRating;
+
+  for (let i = 0; i < wins; i++) {
+    const expected = expectedScore(rating);
+    rating = updateRating(rating, expected, 1);
+  }
+
+  for (let i = 0; i < loses; i++) {
+    const expected = expectedScore(rating);
+    rating = updateRating(rating, expected, 0);
+  }
+
+  return rating;
+};
+
+export const createDummyUsers = (num: number): DummyUser[] => {
+  return Array.from({ length: num }, (_, index): DummyUser => {
+    const id = `42-${(index + 1).toString().padStart(6, '0')}`;
+    const nickname = `user${index + 1}`;
+    const win = Math.floor(Math.random() * 201);
+    const lose = Math.floor(Math.random() * 201);
+    const ladder_win = Math.floor(Math.random() * 201);
+    const ladder_lose = Math.floor(Math.random() * 201);
+    const rating = Math.floor(calculateRating(1000, ladder_win, ladder_lose));
+    const status = Math.floor(Math.random() * 3); // 랜덤하게 0, 1, 2 중 하나를 선택
+
+    return {
+      id,
+      nickname,
+      win,
+      lose,
+      ladder_win,
+      ladder_lose,
+      admin: false,
+      avatarPath: `http://localhost/files/profiles/profile${
+        (index + 1) % 4
+      }.svg`,
+      status,
+      twoFactorAuthenticationSecret: '',
+      isTwoFactorAuthenticationEnabled: false,
+      rating,
+      channels: [],
+    };
+  });
+};
