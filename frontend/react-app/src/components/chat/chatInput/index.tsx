95f3219f411f88af0c20ed66e8bfda044c906f6f
42sangkkim <sangkkim@student.42seoul.kr>
2023-08-29 17:39:49 +0900

    Feat: chat input 추가 #80

---
+++ frontend/react-app/src/components/chat/chatInput/index.tsx
@@ -0,0 +1,53 @@
+import * as S from "./index.styled";
+import { useState } from "react";
+
+export interface ChatInputPropsType {
+  onSendMessage?: (message: string) => void;
+  onGameClick?: () => void;
+  isChannel: boolean;
+}
+
+const ChatInput = ({
+  onSendMessage,
+  onGameClick,
+  isChannel,
+}: ChatInputPropsType) => {
+  const [value, setValue] = useState<string>("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (value.trim().length === 0) return;
+    // TODO: send message
+    if (onSendMessage) {
+      onSendMessage(value);
+    } else {
+      alert(`message: ${value}`);
+    }
+    setValue("");
+  };
+
+  const handleGameClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    // TODO: send game message
+    if (onGameClick) {
+      onGameClick();
+    } else {
+      alert("Let's game together!");
+    }
+  };
+  return (
+    <S.Container>
+      <S.InviteButton onClick={handleGameClick} isChannel={isChannel} />
+      <S.Form onSubmit={sendMessage}>
+        <S.Input value={value} onChange={handleInputChange} />
+        <S.SendButton type="submit" />
+      </S.Form>
+    </S.Container>
+  );
+};
+
+export default ChatInput;
