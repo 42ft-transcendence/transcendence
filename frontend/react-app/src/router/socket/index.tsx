07078ed935c8ebca496435ececfcbe435ac20c56
Y2IN <dpdls0228@daum.net>
2023-08-29 14:55:20 +0900

    Feat: 게임 소켓 추가 #81

--- frontend/react-app/src/router/socket/index.tsx
+++ frontend/react-app/src/router/socket/index.tsx
@@ -1,12 +1,14 @@
 import { ChatType, UserType } from "@type";
 import { chatSocket, chatSocketConnect } from "./chatSocket";
 import * as cookies from "react-cookies";
+import { gameSocket, gameSocketConnect } from "./gameSocket";
 
 const Socket = ({ children }: { children: React.ReactNode }) => {
   const jwt = cookies.load("jwt");
 
   if (!jwt) {
     chatSocket.disconnect();
+    gameSocket.disconnect();
   } else {
     // Init chat socket events
     chatSocket.off("refresh_list");
@@ -22,7 +24,13 @@ const Socket = ({ children }: { children: React.ReactNode }) => {
     // chatSocket.off("get_dm");
     // chatSocket.on("get_dm", () => {});
 
+    gameSocket.off("");
+    gameSocket.on("", () => {
+      console.log("");
+    });
+
     chatSocketConnect(jwt);
+    gameSocketConnect(jwt);
   }
 
   return children;
