0df5e646a514f028e0bfe06151906074856b70d5
42sangkkim <sangkkim@student.42seoul.kr>
2023-08-29 17:09:32 +0900

    Feat: chat item 생성 #80

---
+++ frontend/react-app/src/components/chat/chatListItem/index.styled.ts
@@ -0,0 +1,55 @@
+import { RoleType } from "@src/types";
+import { styled } from "styled-components";
+import Owner from "@assets/icons/role_owner.svg";
+import Admin from "@assets/icons/role_admin.svg";
+
+export const Container = styled.li<{ isMine: boolean }>`
+  display: flex;
+  width: 100%;
+  min-height: 60px;
+  justify-content: ${(props) => (props.isMine ? "flex-end" : "flex-start")};
+`;
+
+export const ProfileImage = styled.img<{ role: RoleType }>`
+  width: 50px;
+  height: 50px;
+  top: 5px;
+  left: 5px;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.colors.floating};
+  box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.25);
+
+  &:after {
+    position: absolute;
+    content: "";
+    width: 24px;
+    height: 24px;
+    left: 30px;
+    background-repeat: no-repeat;
+    display: inline-block;
+    vertical-align: top;
+
+    ${(props) => {
+      switch (props.role) {
+        case "owner":
+          return `background-image: url(${Owner});`;
+        case "admin":
+          return `background-image: url(${Admin});`;
+        default:
+          return `display: none;`;
+      }
+    }}
+  }
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Nickname = styled.h3`
+  height: 20px;
+  margin: 0 0 0 20px;
+  color: ${(props) => props.theme.colors.heavyPurple};
+  font-size: 16px;
+`;
---
+++ frontend/react-app/src/components/chat/chatListItem/index.tsx
@@ -0,0 +1,38 @@
+import { ChatType } from "@src/types";
+import ChatBubble from "@components/chat/chatBubble";
+import * as S from "./index.styled";
+
+export interface ChatListItemPropsType {
+  isMine: boolean;
+  chat: ChatType;
+}
+
+const ChatListItem = ({ isMine, chat }: ChatListItemPropsType) => {
+  if (isMine) {
+    return (
+      <li>
+        <S.Container isMine={isMine}>
+          <ChatBubble message={chat.message.content} isMine={isMine} />
+        </S.Container>
+      </li>
+    );
+  } else {
+    return (
+      <li>
+        <S.Container isMine={isMine}>
+          <S.ProfileImage
+            src={chat.user.avatarPath}
+            role={chat.role}
+            alt={chat.user.nickname}
+          />
+          <S.ContentContainer>
+            <S.Nickname>{chat.user.nickname}</S.Nickname>
+            <ChatBubble message={chat.message.content} isMine={isMine} />
+          </S.ContentContainer>
+        </S.Container>
+      </li>
+    );
+  }
+};
+
+export default ChatListItem;
