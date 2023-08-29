e24185058c3d9ec5fbb7133fff55782c25fd0e6d
42sangkkim <sangkkim@student.42seoul.kr>
2023-08-29 22:29:31 +0900

    Feat: refresh_list, get_dm 이벤트 리스너 설정 #80

--- frontend/react-app/src/router/socket/index.tsx
+++ frontend/react-app/src/router/socket/index.tsx
@@ -1,19 +1,31 @@
-import { ChatType, UserType } from "@type";
+import { ChatType, DirectMessageType, UserType } from "@type";
 import { chatSocket, chatSocketConnect } from "./chatSocket";
 import * as cookies from "react-cookies";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import {
   channelState,
   joinedChannelListState,
   messageListState,
 } from "@src/recoil/atoms/channel";
+import { allUserListState } from "@src/recoil/atoms/common";
+import {
+  dmListState,
+  dmOtherState,
+  joinedDmOtherListState,
+} from "@src/recoil/atoms/directMessage";
 
 const Socket = ({ children }: { children: React.ReactNode }) => {
   const jwt = cookies.load("jwt");
 
+  const setAllUserList = useSetRecoilState(allUserListState);
   const setJoinedChannelList = useSetRecoilState(joinedChannelListState);
   const setMessageList = useSetRecoilState(messageListState);
   const channel = useRecoilValue(channelState);
+  const [joinedDmOtherList, setJoinedDmOtherList] = useRecoilState(
+    joinedDmOtherListState,
+  );
+  const setDmList = useSetRecoilState(dmListState);
+  const dmOther = useRecoilValue(dmOtherState);
 
   if (!jwt) {
     chatSocket.disconnect();
@@ -21,7 +33,7 @@ const Socket = ({ children }: { children: React.ReactNode }) => {
     // Init chat socket events
     chatSocket.off("refresh_list");
     chatSocket.on("refresh_list", (userList: UserType[]) => {
-      console.log("refresh_list", userList);
+      setAllUserList(userList);
     });
 
     chatSocket.off("get_message");
@@ -39,8 +51,26 @@ const Socket = ({ children }: { children: React.ReactNode }) => {
       }
     });
 
-    // chatSocket.off("get_dm");
-    // chatSocket.on("get_dm", () => {});
+    chatSocket.off("get_dm");
+    chatSocket.on("get_dm", (dm: DirectMessageType) => {
+      console.log("dm", dm);
+      if (dmOther?.id === dm.from.id) {
+        setDmList((prev) => [...prev, dm]);
+      } else if (joinedDmOtherList.find((other) => other.id === dm.from.id)) {
+        setJoinedDmOtherList((prev) =>
+          prev.map((other) =>
+            other.id === dm.from.id
+              ? { ...other, hasNewMessages: true }
+              : other,
+          ),
+        );
+      } else {
+        setJoinedDmOtherList((prev) => [
+          ...prev,
+          { ...dm.from, hasNewMessages: true },
+        ]);
+      }
+    });
 
     chatSocketConnect(jwt);
   }
