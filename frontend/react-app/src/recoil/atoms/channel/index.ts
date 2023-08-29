561a6f7f5e4e9c7086b7323a352cc1671b8068a5
42sangkkim <sangkkim@student.42seoul.kr>
2023-08-29 19:03:48 +0900

    Feat: channel 관련 atom 생성 #80

--- frontend/react-app/src/recoil/atoms/channel/index.ts
+++ frontend/react-app/src/recoil/atoms/channel/index.ts
@@ -1,11 +1,34 @@
 import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
-import { JoinedChannelType } from "@type";
+import {
+  ChannelType,
+  JoinedChannelType,
+  MessageType,
+  ParticipantType,
+} from "@type";
 
 const { persistAtom } = recoilPersist();
 
 export const joinedChannelListState = atom<JoinedChannelType[]>({
   key: "joinedChannelListState",
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const channelState = atom<ChannelType | null>({
+  key: "channelState",
+  default: null,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const messageListState = atom<MessageType[]>({
+  key: "messageListState",
+  default: [],
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const participantListState = atom<ParticipantType[]>({
+  key: "participantListState",
   default: [],
   effects_UNSTABLE: [persistAtom],
 });
