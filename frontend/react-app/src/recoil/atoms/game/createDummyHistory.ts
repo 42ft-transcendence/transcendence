9c6961e9576fe5fdbe20dbd108e971e7cc6c66f4
Chanwoong1 <jcw7468@gmail.com>
2023-08-30 16:01:13 +0900

    Fix: 더미 전적 랜덤 날짜 설정 기능 추가 #86

--- frontend/react-app/src/recoil/atoms/game/createDummyHistory.ts
+++ frontend/react-app/src/recoil/atoms/game/createDummyHistory.ts
@@ -10,6 +10,10 @@ export const createDummyHistory = (
   for (let i = 0; i < num; i++) {
     let availableUsers = [...userList]; // userList를 복사하여 원본을 수정하지 않게 합니다.
 
+    const oneMonthInMs = 30 * 24 * 60 * 60 * 1000; // 과거 한 달을 밀리초로
+    const randomTimeAgo = Math.random() * oneMonthInMs; // 랜덤한 밀리초 값 (0부터 과거 한 달 사이)
+    const randomPastTime = new Date().getTime() - randomTimeAgo; // 랜덤한 과거 시간
+
     const randomSeed1 = Math.floor(Math.random() * availableUsers.length);
     const player1 = availableUsers[randomSeed1];
     availableUsers = availableUsers.filter((_, index) => index !== randomSeed1);
@@ -17,7 +21,6 @@ export const createDummyHistory = (
     const randomSeed2 = Math.floor(Math.random() * availableUsers.length);
     const player2 = availableUsers[randomSeed2];
 
-    const currentTime = new Date().getTime();
     let player1Score = Math.floor(Math.random() * 5);
     let player2Score = Math.floor(Math.random() * 5);
     if (player1Score < player2Score && player2Score !== 5) {
@@ -40,7 +43,7 @@ export const createDummyHistory = (
       id: i + 1,
       player1Score: player1Score,
       player2Score: player2Score,
-      createdAt: new Date(currentTime + i),
+      createdAt: new Date(randomPastTime),
       player1,
       player2,
       player1ScoreChange,
