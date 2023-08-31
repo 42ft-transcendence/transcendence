5d8a8a20df1d0c374b389fc07cdcfdec7bb4624c
Y2IN <dpdls0228@daum.net>
2023-08-31 14:07:32 +0900

    Feat: 게임 방 사이드 바 기능 구현#86

--- frontend/react-app/src/components/common/sideBar/gameSideBar/index.tsx
+++ frontend/react-app/src/components/common/sideBar/gameSideBar/index.tsx
@@ -1,18 +1,37 @@
 import { ButtonList, IconButtonProps } from "@src/components/buttons";
 import * as DS from "../index.styled";
+// import { roomNameBox } from "./index.styled";
 import RateDoughnutChart from "@src/components/charts/rateDoughnutChart";
 import { useRecoilState } from "recoil";
 import { userDataState } from "@src/recoil/atoms/common";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { gameRoomName } from "@src/recoil/atoms/game";
 // import { createGameRoomModalState } from "@src/recoil/atoms/modal";
 
 const GameSideBar = () => {
   const [userData] = useRecoilState(userDataState);
+  const [roomTitle] = useRecoilState(gameRoomName);
   // const [, setCreateGameRoom] = useRecoilState(createGameRoomModalState);
   const [, setLeaveGameRoom] = useState(false);
   const navigate = useNavigate();
   const iconButtons: IconButtonProps[] = [
+    {
+      title: "준비 하기",
+      iconSrc: "",
+      onClick: () => {
+        console.log("준비 하기");
+      },
+      theme: "LIGHT",
+    },
+    {
+      title: "준비 취소",
+      iconSrc: "",
+      onClick: () => {
+        console.log("준비취소");
+      },
+      theme: "LIGHT",
+    },
     {
       title: "방 나가기",
       iconSrc: "",
@@ -22,31 +41,20 @@ const GameSideBar = () => {
       },
       theme: "LIGHT",
     },
-    // {
-    //   title: "준비하기",
-    //   iconSrc: "",
-    //   onClick: () => {
-    //     console.log("랭킹전 참가");
-    //   },
-    //   theme: "LIGHT",
-    // },
-    // {
-    //   title: "둘러보기",
-    //   iconSrc: "",
-    //   onClick: () => {
-    //     console.log("둘러보기");
-    //   },
-    //   theme: "LIGHT",
-    // },
   ];
-
+  console.log(roomTitle);
   return (
-    <DS.Container>
-      <ButtonList buttons={iconButtons} />
-      <br />
-      <DS.TitleBox>내 전적</DS.TitleBox>
-      <RateDoughnutChart userData={userData} />
-    </DS.Container>
+    <>
+      <DS.Container>
+        <DS.roomNameBox>{roomTitle}</DS.roomNameBox>
+        <DS.boxWrapper>
+          <DS.TitleBox>내 전적</DS.TitleBox>
+          <RateDoughnutChart userData={userData} />
+          <br />
+          <ButtonList buttons={iconButtons} />
+        </DS.boxWrapper>
+      </DS.Container>
+    </>
   );
 };
 
--- frontend/react-app/src/components/common/sideBar/index.styled.ts
+++ frontend/react-app/src/components/common/sideBar/index.styled.ts
@@ -11,14 +11,31 @@ export const Container = styled.div`
   align-items: center;
 `;
 
+export const boxWrapper = styled.div`
+  display: flex;
+  height: calc(100% - 100px);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const TitleBox = styled.div`
   display: flex;
   width: 80%;
   height: 30px;
   align-items: center;
+  justify-content: center;
+
   font-size: 26px;
   font-weight: bold;
   font-family: inter;
   color: ${(props) => props.theme.colors.freezePurple};
   cursor: default;
+`;
+
+export const roomNameBox = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  height: 50px;
+  color: ${(props) => props.theme.colors.freezePurple};
 `;
