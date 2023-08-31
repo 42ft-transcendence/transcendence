aded196821bb351bdefcd4566450761023b84eb6
Chanwoong1 <jcw7468@gmail.com>
2023-08-31 21:40:59 +0900

    Fix: 텍스트 입력창 자동완성 시 배경 깨지는 현상 수정 #103

--- frontend/react-app/src/components/inputBox/index.styled.ts
+++ frontend/react-app/src/components/inputBox/index.styled.ts
@@ -13,4 +13,17 @@ export const InputBoxWrapper = styled.input`
   border-bottom: 2px solid ${Theme.colors.freezePurple};
   color: ${Theme.colors.freezePurple};
   margin-top: 50px;
+
+  /* 자동 완성 배경색 및 폰트 스타일 제어 */
+  &:-webkit-autofill,
+  &:-webkit-autofill:hover,
+  &:-webkit-autofill:focus,
+  &:-webkit-autofill:active {
+    transition: background-color 5000s ease-in-out 0s;
+    -webkit-text-fill-color: ${(props) =>
+      props.theme.colors.freezePurple}; /* 텍스트 색상 */
+    font-size: 20px; /* 폰트 크기 */
+    font-family: inter; /* 폰트 종류 */
+    font-weight: bold; /* 폰트 두께 */
+  }
 `;
