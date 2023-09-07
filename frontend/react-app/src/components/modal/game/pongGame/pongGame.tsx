872f4f248c0510b44076cdec99b648e7563454ba
Y2IN <dpdls0228@daum.net>
2023-09-07 12:53:26 +0900

    Feat: 점수 구현 #115

--- frontend/react-app/src/components/modal/game/pongGame/pongGame.tsx
+++ frontend/react-app/src/components/modal/game/pongGame/pongGame.tsx
@@ -1,7 +1,7 @@
 // PongGame.tsx
 import React, { useEffect } from "react";
 import NormalMapSvg from "@assets/maps/mapNormal.svg";
-// import { ballLocationType } from "@src/types/game.type";
+import { ballLocationType } from "@src/types/game.type";
 import { useRecoilState } from "recoil";
 import { gameRoomInfoState, gameRoomURLState } from "@src/recoil/atoms/game";
 import { userDataState } from "@src/recoil/atoms/common";
@@ -61,22 +61,22 @@ const PongGame: React.FC = () => {
       right: 0,
     };
 
-    // const ball: ballLocationType = {
-    //   x: cvs.width / 2,
-    //   y: cvs.height / 2,
-    //   radius: 10,
-    //   speed: 5,
-    //   velocityX: 5,
-    //   velocityY: 5,
-    //   color: "WHITE",
-    //   top: 0,
-    //   bottom: 0,
-    //   left: 0,
-    //   right: 0,
-    //   score: 0,
-    //   height: 0,
-    //   width: 0,
-    // };
+    const ball: ballLocationType = {
+      x: cvs.width / 2,
+      y: cvs.height / 2,
+      radius: 10,
+      speed: 8,
+      velocityX: 5,
+      velocityY: 0,
+      color: "WHITE",
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      score: 0,
+      height: 0,
+      width: 0,
+    };
 
     function drawpaddle(
       x: number,
@@ -115,6 +115,20 @@ const PongGame: React.FC = () => {
     //   ctx.fill();
     // }
 
+    function drawBall(x: number, y: number, color: string) {
+      if (!ctx) {
+        console.error("Canvas context is null.");
+        return;
+      }
+      // const radius = Math.sqrt(vecX * vecX + vecY * vecY);
+      const radius = 10;
+      // console.log("radius", radius);
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+      ctx.closePath();
+      ctx.fill();
+    }
     function render() {
       if (!ctx) {
         console.error("Canvas context is null.");
@@ -124,13 +138,14 @@ const PongGame: React.FC = () => {
 
       //draw score
       drawText(user.score, cvs.width / 4, cvs.height / 5, "WHITE");
-      // drawText(com.score, (3 * cvs.width) / 4, cvs.height / 5, "WHITE");
+      drawText(com.score, (3 * cvs.width) / 4, cvs.height / 5, "WHITE");
 
       drawpaddle(user.x, user.y, user.width, user.height, user.color);
       drawpaddle(com.x, com.y, com.width, com.height, com.color);
 
       //draw the ball
-      // drawBall(ball.x, ball.y, ball.radius, ball.color);
+
+      drawBall(ball.x, ball.y, ball.color);
     }
 
     // control the user paddle
@@ -140,70 +155,6 @@ const PongGame: React.FC = () => {
       const rect = cvs.getBoundingClientRect();
       user.y = evt.clientY - rect.top - user.height / 2;
     }
-
-    // collision detection
-    // function collision(b: ballLocationType, p: ballLocationType) {
-    //   // console.log("b", b);
-    //   b.top = b.y - b.radius;
-    //   b.bottom = b.y + b.radius;
-    //   b.left = b.x - b.radius;
-    //   b.right = b.x + b.radius;
-
-    //   p.top = p.y;
-    //   p.bottom = p.y + p.height;
-    //   p.left = p.x;
-    //   p.right = p.x + p.width;
-
-    //   return (
-    //     b.right > p.left &&
-    //     b.bottom > p.top &&
-    //     b.left < p.right &&
-    //     b.top < p.bottom
-    //   );
-    // }
-
-    // // update : pos, mov, score
-    // function update() {
-    //   if (!ctx) {
-    //     console.error("Canvas context is null.");
-    //     return;
-    //   }
-
-    //   ball.x += ball.velocityX;
-    //   ball.y += ball.velocityY;
-
-    //   const computerLevel = 0.1;
-    //   com.y += (ball.y - (com.y + com.height / 2)) * computerLevel;
-    //   if (ball.y + ball.radius > cvs.height || ball.y - ball.radius < 0) {
-    //     ball.velocityY = -ball.velocityY;
-    //   }
-    //   const player = ball.x < cvs.width / 2 ? user : com;
-    //   if (collision(ball, player)) {
-    //     // ball.velocityX = -ball.velocityX;
-    //     const collide = ball.y - (player.y + player.height / 2);
-
-    //     const collidePoint = collide / (player.height / 2);
-    //     const angleRad = (collidePoint * Math.PI) / 4;
-    //     const direction = ball.x < cvs.width / 2 ? 1 : -1;
-    //     ball.velocityX = direction * ball.speed * Math.cos(angleRad);
-    //     ball.velocityY = ball.speed * Math.sin(angleRad);
-    //     ball.speed += 0.1;
-    //   }
-    //   if (ball.x - ball.radius < 0) {
-    //     com.score++;
-    //     resetBall();
-    //   } else if (ball.x + ball.radius > cvs.width) {
-    //     user.score++;
-    //     resetBall();
-    //   }
-    // }
-
-    // function resetBall() {
-    //   ball.x = cvs.width / 2;
-    //   ball.y = cvs.height / 2;
-    //   ball.speed = 5;
-    //   ball.velocityX = -ball.velocityX;
-    // }
 
     let animationFrameId: number;
 
@@ -234,6 +185,23 @@ const PongGame: React.FC = () => {
       data.userIndex === 0
         ? (com.y = data.gameData.leftPaddle)
         : (com.y = data.gameData.rightPaddle);
+      if (data.userIndex === 0) {
+        user.score = data.gameData.score[0];
+        com.score = data.gameData.score[1];
+      } else {
+        user.score = data.gameData.score[1];
+        com.score = data.gameData.score[0];
+      }
+      // ? {user.score = data.gameData.score[0], com.score = data.gameData.sxore[1]}
+      // : (com.score = data.gameData.sxore[1]);
+      ball.x = data.gameData.ballX;
+      ball.y = data.gameData.ballY;
+      ball.velocityX = data.gameData.ballVecX;
+      ball.velocityY = data.gameData.ballVecY;
+      ball.speed = data.gameData.ballSpeed;
+
+      // console.log("gameProcess", data.gameData);
+      // console.log("ball", ball);
     });
 
     // 이펙트 클린업 함수
