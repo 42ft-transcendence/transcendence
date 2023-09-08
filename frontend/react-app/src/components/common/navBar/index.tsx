dbc6c425c658b8dc302c78e63c83f0235c767501
Chanwoong1 <jcw7468@gmail.com>
2023-09-09 03:01:39 +0900

    Feat: 페이지 이동할 때마다 최신 유저 데이터 요청 #139

--- frontend/react-app/src/components/common/navBar/index.tsx
+++ frontend/react-app/src/components/common/navBar/index.tsx
@@ -1,7 +1,11 @@
 import * as S from "./index.styled";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { LowerTabList, SettingOptionModal, UpperTabList } from "./container";
-import { isFirstLoginState, showProfileState } from "@src/recoil/atoms/common";
+import {
+  isFirstLoginState,
+  showProfileState,
+  userDataState,
+} from "@src/recoil/atoms/common";
 import ProfileModal from "@src/components/modal/profile";
 import {
   battleActionModalState,
@@ -15,12 +19,14 @@ import FirstLoginModal from "@src/components/modal/login/firstLoginModal";
 import { useEffect } from "react";
 import useInitializeState from "@src/hooks/useInitializeState";
 import { gameRoomURLState } from "@src/recoil/atoms/game";
+import { getUser } from "@src/api";
 
 export interface NavBarPropsType {
   currentPath: string;
 }
 
 const NavBar = () => {
+  const setUserData = useSetRecoilState(userDataState);
   const [showProfile] = useRecoilState(showProfileState);
   const [battleActionModal] = useRecoilState(battleActionModalState);
   const [gameAlertModal] = useRecoilState(gameAlertModalState);
@@ -29,6 +35,7 @@ const NavBar = () => {
     useRecoilState<boolean>(isFirstLoginState);
   const [gameRoomURL, setGameRoomURL] = useRecoilState(gameRoomURLState);
   const initializer = useInitializeState();
+  const currentPath = window.location.pathname;
 
   useEffect(() => {
     if (!isFirstLogin) return;
@@ -39,10 +46,20 @@ const NavBar = () => {
       setIsFirstLogin(false);
     }, 5000);
     // ! 만약 게임 URL이 남아있다면, 그 방이 남아있는지 확인
+    console.log("gameRoomURL in navBar", gameRoomURL);
     setGameRoomURL("");
     // ? 남아있는데 대기중이라면 게임방으로 이동시키기
     // ? 남아있지 않다면 게임 유무 판단(가장 최근 전적이 몰수패라면 연결이 끊겼다고 판단할 수 있을듯함)으로 기록으로 이동하시겠습니까? or 그냥 기본 동작
   }, [isFirstLogin]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      await getUser()
+        .then((res) => setUserData(res.data))
+        .catch((err) => void err);
+    };
+    getUserData();
+  }, [currentPath]);
 
   return (
     <S.Container>
