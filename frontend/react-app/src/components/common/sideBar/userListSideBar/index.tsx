8f676035cd28c62c78d0e93d5ff8d94353317672
Chanwoong1 <jcw7468@gmail.com>
2023-09-10 00:33:04 +0900

    Remove: 사용하지 않는 리코일 상태 제거 #147

--- frontend/react-app/src/components/common/sideBar/userListSideBar/index.tsx
+++ frontend/react-app/src/components/common/sideBar/userListSideBar/index.tsx
@@ -4,8 +4,7 @@ import { useRecoilState } from "recoil";
 import { allUserListState } from "@src/recoil/atoms/common";
 import { UserStatusCounts } from "@src/types/user.type";
 import { createDummy } from "@src/api";
-import { createDummyHistory } from "@src/recoil/atoms/game/createDummyHistory";
-import { matchHistoryListState } from "@src/recoil/atoms/game";
+// import { createDummyHistory } from "@src/recoil/atoms/game/createDummyHistory";
 
 interface UserListSideBarProps {
   onAllUsersClick: () => void;
@@ -27,7 +26,6 @@ const UserListSideBar: React.FC<UserListSideBarProps> = ({
   currentClick,
 }) => {
   const [allUserList] = useRecoilState(allUserListState);
-  const [, setMatchHistoryList] = useRecoilState(matchHistoryListState);
 
   // TODO: text2에 "0"인 경우는 추가 구현 사항
   const userButtonList: DoubleTextButtonProps[] = [
@@ -75,20 +73,20 @@ const UserListSideBar: React.FC<UserListSideBarProps> = ({
       },
       theme: "LIGHT",
     },
-    {
-      text1: "더미 전적 생성",
-      text2: "5000",
-      onClick: () => {
-        console.log("더미 전적 생성");
-        console.log(
-          "createDummyHistory",
-          createDummyHistory(allUserList, 5000),
-        );
-        const dummyHistory = createDummyHistory(allUserList, 5000);
-        setMatchHistoryList(dummyHistory);
-      },
-      theme: "LIGHT",
-    },
+    // {
+    //   text1: "더미 전적 생성",
+    //   text2: "5000",
+    //   onClick: () => {
+    //     console.log("더미 전적 생성");
+    //     console.log(
+    //       "createDummyHistory",
+    //       createDummyHistory(allUserList, 5000),
+    //     );
+    //     const dummyHistory = createDummyHistory(allUserList, 5000);
+    //     setMatchHistoryList(dummyHistory);
+    //   },
+    //   theme: "LIGHT",
+    // },
   ];
 
   return (
