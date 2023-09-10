5bdd1ff92dd4e80a83ca7c0ae204ab6834c6d9e5
sangkkim <sangkkim@student.42seoul.kr>
2023-09-10 21:16:43 +0900

    Fix: dm 창으로 아동 후 프로필 닫음 #163

--- frontend/react-app/src/components/modal/profile/container.tsx
+++ frontend/react-app/src/components/modal/profile/container.tsx
@@ -65,6 +65,11 @@ export const ProfileButtonActions = ({ role }: ProfileButtonActionsProps) => {
   const [isBlocked, setIsBlocked] = useState<boolean>(false);
   const setGameRoomURL = useSetRecoilState(gameRoomURLState);
   const navigate = useNavigate();
+  const closeProfile = ProfileModalOnClickHandler(
+    setShowProfile,
+    false,
+    {} as UserType,
+  );
 
   // 친구 상태인지 확인
 
@@ -177,7 +182,10 @@ export const ProfileButtonActions = ({ role }: ProfileButtonActionsProps) => {
     },
     {
       label: "DM 보내기",
-      action: () => navigate(`/direct-message/${user.user.id}`),
+      action: () => {
+        navigate(`/direct-message/${user.user.id}`);
+        closeProfile();
+      },
       src: SendMessageIcon,
     },
     {
