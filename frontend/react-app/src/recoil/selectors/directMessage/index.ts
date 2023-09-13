cf6f9cf10cd67f297795d20b808b40398e742fbc
42sangkkim <sangkkim@student.42seoul.kr>
2023-09-13 15:23:14 +0900

    Feat: dmUserList selector #174

--- frontend/react-app/src/recoil/selectors/directMessage/index.ts
+++ frontend/react-app/src/recoil/selectors/directMessage/index.ts
@@ -1,7 +1,10 @@
 import { allUserListState } from "@recoil/atoms/common";
-import { dmOtherState } from "@recoil/atoms/directMessage";
+import {
+  dmOtherState,
+  joinedDmOtherListState,
+} from "@recoil/atoms/directMessage";
 import { selector } from "recoil";
-import { UserType } from "@src/types";
+import { JoinedDmOtherType, UserType } from "@src/types";
 
 export const dmUserState = selector<UserType | null>({
   key: "dmUserState",
@@ -12,5 +15,23 @@ export const dmUserState = selector<UserType | null>({
     const dmUser = allUserList.find((user) => user.id === dmOther?.id) ?? null;
 
     return dmUser;
+  },
+});
+
+export const dmUserListState = selector<JoinedDmOtherType[]>({
+  key: "dmUserListState",
+  get: ({ get }) => {
+    const allUserList = get(allUserListState);
+    const joinedDmOtherList = get(joinedDmOtherListState);
+
+    const dmUserList = joinedDmOtherList.map((item) => {
+      const user = allUserList.find((user) => user.id === item.id) ?? item;
+      return {
+        ...user,
+        hasNewMessages: item.hasNewMessages,
+      };
+    });
+
+    return dmUserList;
   },
 });
