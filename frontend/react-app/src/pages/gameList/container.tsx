090d9c8c1b378ab16557d6bf9b5b9d9a5026f8a5
42sangkkim <sangkkim@student.42seoul.kr>
2023-09-13 22:27:02 +0900

    Feat: game list page 필터, 검색 구현 #174

--- frontend/react-app/src/pages/gameList/container.tsx
+++ frontend/react-app/src/pages/gameList/container.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as DS from "../index.styled";
 import * as S from "./index.styled";
 import { useRecoilState } from "recoil";
@@ -19,6 +19,9 @@ export const GameListContent = ({
 }: GameListContentProps) => {
   const [search, setSearch] = useState<string>("");
   const [gameRoomList] = useRecoilState(gameRoomListState);
+  const [filteredRoomList, setFilteredRoomList] = useState<GameRoomInfoType[]>(
+    [],
+  );
   const [sortState, setSortState] = useState<string>("전체 방");
 
   const handleOnClick = (props: GameRoomInfoType) => {
@@ -26,6 +29,25 @@ export const GameListContent = ({
     setSelectGameRoom(props);
     setIsOpenEnterModal(true);
   };
+
+  useEffect(() => {
+    const searchedRoomList = gameRoomList.filter((room) =>
+      room.roomName.includes(search),
+    );
+    if (sortState === "전체 방") {
+      setFilteredRoomList(
+        searchedRoomList.filter((room) => room.roomType !== "PRIVATE"),
+      );
+    } else if (sortState === "공개 방") {
+      setFilteredRoomList(
+        searchedRoomList.filter((room) => room.roomType === "PUBLIC"),
+      );
+    } else if (sortState === "비밀 방") {
+      setFilteredRoomList(
+        searchedRoomList.filter((room) => room.roomType === "PROTECTED"),
+      );
+    }
+  }, [gameRoomList, search, sortState]);
 
   return (
     <DS.ContentArea>
@@ -35,46 +57,40 @@ export const GameListContent = ({
         setSearch={setSearch}
         sortState={sortState}
         setSortState={setSortState}
-        sortOptions={["전체 방", "공개 방", "비공개 방"]}
+        sortOptions={["전체 방", "공개 방", "비밀 방"]}
         placeholder="방 이름을 입력하세요"
       />
       <S.GameRoomCardContainer>
-        {gameRoomList
-          .filter(
-            (gameRoom) =>
-              gameRoom.roomType === "PUBLIC" ||
-              gameRoom.roomType === "PROTECTED",
-          )
-          .map((gameRoom) => (
-            <S.GameRoomCard
-              key={gameRoom.roomURL}
-              $roomType={gameRoom.roomType}
-              onClick={() => {
-                if (gameRoom.status === GameRoomStatus.GAMING) return;
-                handleOnClick(gameRoom);
-              }}
-            >
-              <S.GameRoomCardLeft>
-                <S.GameRoomTitle>
-                  {gameRoom.roomName !== "" ? gameRoom.roomName : "빠른 대전"}
-                </S.GameRoomTitle>
-                <S.GameRoomOption>
-                  {gameRoom.map} | {gameRoom.gameMode} |{" "}
-                  {gameRoom.roomOwner.nickname}
-                </S.GameRoomOption>
-              </S.GameRoomCardLeft>
-              <S.GameRoomCardRight>
-                <S.GameRoomNumOfPeople>
-                  {gameRoom.numberOfParticipants} / 2
-                </S.GameRoomNumOfPeople>
-                <S.GameRoomStatus>
-                  {gameRoom.status === GameRoomStatus.WAITING
-                    ? "대기중"
-                    : "게임중"}
-                </S.GameRoomStatus>
-              </S.GameRoomCardRight>
-            </S.GameRoomCard>
-          ))}
+        {filteredRoomList.map((gameRoom) => (
+          <S.GameRoomCard
+            key={gameRoom.roomURL}
+            $roomType={gameRoom.roomType}
+            onClick={() => {
+              if (gameRoom.status === GameRoomStatus.GAMING) return;
+              handleOnClick(gameRoom);
+            }}
+          >
+            <S.GameRoomCardLeft>
+              <S.GameRoomTitle>
+                {gameRoom.roomName !== "" ? gameRoom.roomName : "빠른 대전"}
+              </S.GameRoomTitle>
+              <S.GameRoomOption>
+                {gameRoom.map} | {gameRoom.gameMode} |{" "}
+                {gameRoom.roomOwner.nickname}
+              </S.GameRoomOption>
+            </S.GameRoomCardLeft>
+            <S.GameRoomCardRight>
+              <S.GameRoomNumOfPeople>
+                {gameRoom.numberOfParticipants} / 2
+              </S.GameRoomNumOfPeople>
+              <S.GameRoomStatus>
+                {gameRoom.status === GameRoomStatus.WAITING
+                  ? "대기중"
+                  : "게임중"}
+              </S.GameRoomStatus>
+            </S.GameRoomCardRight>
+          </S.GameRoomCard>
+        ))}
       </S.GameRoomCardContainer>
     </DS.ContentArea>
   );
